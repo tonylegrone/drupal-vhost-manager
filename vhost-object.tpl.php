1998b2a3c019d98ce522c083451d3aaf91519384
Tony Legrone <tony@tonylegrone.com>
2013-07-19 02:40:17 -0500

    Fixed whitespace errors

--- vhost-object.tpl.php
+++ vhost-object.tpl.php
@@ -2,9 +2,9 @@
 <VirtualHost *:80>
   DocumentRoot "<?php echo $object['document_root'] ?>"
   ServerName <?php echo $object['server_name'] ?>
-  
+
   <?php echo $object['server_alias'] ?>
-  
+
   ErrorLog "<?php echo variable_get('vhost_log_path') . $object['server_name'] ?>/error_log"
   CustomLog "<?php echo variable_get('vhost_log_path') . $object['server_name'] ?>/access_log" common
 </VirtualHost>
