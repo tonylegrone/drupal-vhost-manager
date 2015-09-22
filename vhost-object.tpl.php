80487a4cdef05397d2630c0c072a1e92d454d3b2
Tony Legrone <tony@tonylegrone.com>
2015-09-22 12:48:43 -0500

    Add support for SSL

--- vhost-object.tpl.php
+++ vhost-object.tpl.php
@@ -1,3 +1,21 @@
+<?php if ($object['support_ssl']): ?>
+
+<VirtualHost *:<?php echo variable_get('vhost_ssl_port', '443'); ?>>
+  SSLEngine on
+
+  SSLCertificateFile <?php echo $object['ssl_cert_path'] ? $object['ssl_cert_path'] : variable_get('vhost_ssl_cert_path', NULL); ?>
+
+  SSLCertificateKeyFile <?php echo $object['ssl_key_path'] ? $object['ssl_key_path'] : variable_get('vhost_ssl_key_path', NULL); ?>
+
+
+  DocumentRoot "<?php echo $object['document_root'] ?>"
+  ServerName <?php echo $object['server_name'] ?>
+
+  <?php echo $object['server_alias'] ?>
+
+</VirtualHost>
+
+<?php endif; ?>
 
 <VirtualHost *:<?php echo variable_get('vhost_port', '80'); ?>>
   DocumentRoot "<?php echo $object['document_root'] ?>"
@@ -9,6 +27,7 @@
     ErrorLog "<?php echo variable_get('vhost_log_path') . $object['server_name'] ?>/error_log"
     CustomLog "<?php echo variable_get('vhost_log_path') . $object['server_name'] ?>/access_log" common
   <?php endif; ?>
+
 </VirtualHost>
 
 <Directory "<?php echo $object['document_root']; ?>">
